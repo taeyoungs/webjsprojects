a5cdee8650b62146e90f35a118eea379f67cfc6c
taeyoungs <xoxodudwkd@gmail.com>
2021-03-17 15:04:50 +0900

    #9.2 Add Delete Transactions

---
+++ expense-tracker/index.js
@@ -0,0 +1,97 @@
+const balance = document.getElementById('balance');
+const plus = document.getElementById('money-plus');
+const minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
+let dummy = [
+  { id: uuidv4(), type: '현금', amount: 12000 },
+  { id: uuidv4(), type: '마우스', amount: -6000 },
+  { id: uuidv4(), type: '용돈', amount: 3000 },
+  { id: uuidv4(), type: '키보드', amount: -1000 },
+];
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount > 0 ? '+' : '-';
+
+  const item = document.createElement('li');
+
+  if (transaction.amount > 0) {
+    item.classList.add('plus');
+  } else {
+    item.classList.add('minus');
+  }
+
+  item.innerHTML = `
+        ${transaction.type}<span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn" onclick="removeTransaction('${
+          transaction.id
+        }')">삭제</button>
+    `;
+
+  list.appendChild(item);
+}
+
+function updateValue() {
+  const amounts = dummy.reduce((acc, current) => acc + current.amount, 0);
+
+  const income = dummy.reduce(
+    (acc, current) => (current.amount > 0 ? acc + current.amount : acc),
+    0
+  );
+
+  const outcome =
+    -1 *
+    dummy.reduce(
+      (acc, current) => (current.amount < 0 ? acc + current.amount : acc),
+      0
+    );
+
+  balance.innerHTML = `&#8361;${amounts}`;
+  plus.innerHTML = `&#8361;${income}`;
+  minus.innerHTML = `&#8361;${outcome}`;
+}
+
+function removeTransaction(id) {
+  dummy = dummy.filter((item) => item.id !== id);
+
+  init();
+}
+
+function init() {
+  list.innerHTML = '';
+
+  dummy.forEach((item) => addTransactionDOM(item));
+
+  updateValue();
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const item = {
+    id: uuidv4(),
+    type: text.value,
+    amount: +amount.value,
+  };
+
+  dummy.push(item);
+  addTransactionDOM(item);
+  updateValue();
+
+  text.value = '';
+  amount.value = '';
+});
+
+init();
--- notes/9_expense_tracker/92_Add_Delete_Transactions.md
+++ notes/9_expense_tracker/92_Add_Delete_Transactions.md
@@ -1,33 +1,3 @@
-# 90 Add HTML and CSS
-
-## log
-
-- 가계부 HTML, CSS 추가
-
-## tips
-
-## issue
-
-- none
-
-## improving
-
-# 91 Show Items and Display Amounts
-
-## log
-
-- 내역을 리스트 안에 집어넣는 addTransactionDOM 함수 작성
-- 작성한 함수와 dummy data를 통해 올바르게 동작하는지 확인
-- addTransactionDOM 함수가 동작할 때마다 총 금액, 수입, 지출을 업데이트할 함수 작성
-
-## tips
-
-## issue
-
-- none
-
-## improving
-
 # 92 Add Delete Transactions
 
 ## log
